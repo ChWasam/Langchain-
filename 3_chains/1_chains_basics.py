623c8455c3235013e33fc0246f1b4e89d05b8bd3
ChWasam <wasam.developer@gmail.com>
2024-09-18 00:16:38 +0500

    Done with chain basics Example

--- 3_chains/1_chains_basics.py
+++ 3_chains/1_chains_basics.py
@@ -2,13 +2,18 @@
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Load environment variables from .env
 load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                                client_options=None,
+                                transport=None,
+                                additional_headers=None,
+                                client=None,
+                                async_client=None)
 
 # Define prompt templates (no need for separate Runnable chains)
 prompt_template = ChatPromptTemplate.from_messages(
@@ -20,6 +25,8 @@ prompt_template = ChatPromptTemplate.from_messages(
 
 # Create the combined chain using LangChain Expression Language (LCEL)
 chain = prompt_template | model | StrOutputParser()
+
+#  What StrOutputParser() does is basically like every time we get the result we do .content. That is exactly string parser does. 
 # chain = prompt_template | model
 
 # Run the chain
