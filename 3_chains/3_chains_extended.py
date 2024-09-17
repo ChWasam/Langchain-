b8e0f2bdcc52b2d2c5e7adfe902dd642728e52bb
ChWasam <wasam.developer@gmail.com>
2024-09-18 01:27:08 +0500

    Extended chains

--- 3_chains/3_chains_extended.py
+++ 3_chains/3_chains_extended.py
@@ -2,13 +2,18 @@ from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableLambda
-from langchain_openai import ChatOpenAI
+from langchain_google_genai  import ChatGoogleGenerativeAI
 
 # Load environment variables from .env
 load_dotenv()
 
-# Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# Create a ChatGoogleGenerativeAI model
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                                client_options=None,
+                                transport=None,
+                                additional_headers=None,
+                                client=None,
+                                async_client=None)
 
 # Define prompt templates
 prompt_template = ChatPromptTemplate.from_messages(
@@ -21,6 +26,12 @@ prompt_template = ChatPromptTemplate.from_messages(
 # Define additional processing steps using RunnableLambda
 uppercase_output = RunnableLambda(lambda x: x.upper())
 count_words = RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")
+
+#  As we will become more practical we can make api calls 
+# other examples of RunnableLambda are
+# 1. RunnableLambda(lambda x: x.upper())
+# 2. RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")
+
 
 # Create the combined chain using LangChain Expression Language (LCEL)
 chain = prompt_template | model | StrOutputParser() | uppercase_output | count_words
