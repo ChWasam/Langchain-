b4c25636f912d4ee98b6ef104ef833f7b6e02cf0
ChWasam <wasam.developer@gmail.com>
2024-09-18 17:24:43 +0500

    Rag Basics

--- 4_rag/1b_rag_basics.py
+++ 4_rag/1b_rag_basics.py
@@ -16,17 +16,35 @@ db = Chroma(persist_directory=persistent_directory,
 
 # Define the user's question
 query = "Who is Odysseus' wife?"
+# -----------------------------------------------------
+# query = "Who is wasam?" 
+# UserWarning: No relevant docs were retrieved using the relevance score threshold 0.9
 
 # Retrieve relevant documents based on the query
 retriever = db.as_retriever(
     search_type="similarity_score_threshold",
-    search_kwargs={"k": 3, "score_threshold": 0.9},
+    search_kwargs={"k": 3, "score_threshold": 0.4},
+    #  Heigher the score_threshold, the more similar the documents will be to the query
+    # "k": 3=> What it's going to do is it's going to return the top 3 most relevant documents
 )
+#  In RAG if you are not getting results you might be too strict with your score_threshold
+
 relevant_docs = retriever.invoke(query)
+# relevant_docs is list
 
 # Display the relevant results with metadata
+#  All we are doing in this code is we are just printing the relevant documents
 print("\n--- Relevant Documents ---")
 for i, doc in enumerate(relevant_docs, 1):
     print(f"Document {i}:\n{doc.page_content}\n")
     if doc.metadata:
         print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
+
+
+
+#  The enumerate() function in Python allows you to loop over an iterable (like a list, tuple, or string) and keep track of both the index and the element at the same time. This is particularly useful when you want to know the position of an element as you iterate through it.
+
+# In a for loop, when you write for i, doc in enumerate(relevant_docs, 1), you're using tuple unpacking. The enumerate() function returns a tuple for each iteration where:
+# The first element of the tuple is the index (i).
+# The second element of the tuple is the current document object (doc).
+#  More detail on notion
